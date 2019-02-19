d56ed206075a943788ccd93e7ac58aeaebe5f018
wumaoqiang <wumaoqiang@vip.qq.com>
2019-02-19 10:31:13 +0800

    first commit myinfo

---
+++ pages/index/index.js
@@ -0,0 +1,59 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    name: '吴茂强',
+    windowHeight: '',
+    position: 'Android开发工程师',
+    school: '烟台大学 | 电子信息科学与技术 | 本科',
+    xx: '500px',
+    isCheckedSkill: false,
+    isCheckedWork: true,
+    isCheckedProject: false,
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function() {
+    var that = this
+    wx.getSystemInfo({
+      success(res) {
+
+        that.setData({
+          windowHeight: res.windowHeight-320
+        })
+        console.log(that.data.windowHeight)
+
+      }
+    })
+
+
+  },
+  skill: function() {
+    this.setData({
+      isCheckedSkill: true,
+      isCheckedWork: false,
+      isCheckedProject: false,
+    })
+  },
+  work: function() {
+    this.setData({
+      isCheckedSkill: false,
+      isCheckedWork: true,
+      isCheckedProject: false,
+    })
+  },
+  project: function() {
+    this.setData({
+      isCheckedSkill: false,
+      isCheckedWork: false,
+      isCheckedProject: true,
+    })
+  }
+
+})
